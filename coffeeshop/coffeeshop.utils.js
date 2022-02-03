d356273665232021ed5be005394bde31ddb50fa5
SeulkiLee <tmfrl3120@gmail.com>
2022-02-04 03:12:07 +0900

    delete

--- coffeeshop/coffeeshop.utils.js
+++
@@ -1,7 +0,0 @@
-export const processCategory = (slug) => {
-    const categories = caption.match(/#[\w]+/g) || [];
-    return categories.map((category) => ({
-        where: { category },
-        create: { category },
-    }));
-};
